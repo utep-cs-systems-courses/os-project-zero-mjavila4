a96bcb6d8f7625d1d063fb4a76cd66898cb4c46b
mjavila4 <mjavila4@miners.utep.edu>
2022-01-26 09:03:38 -0700

    Working occurences

--- src/wordCount.py
+++ src/wordCount.py
@@ -1,15 +1,6 @@
 import sys
 import re
 
-
-def overlap(word, oList):
-    i = 0
-    for string in oList:
-        if word == string:
-            i += 1
-    return i
-
-
 fileName = sys.argv[1]
 file = open('../' + fileName, "r")
 fileParse = open('../' + fileName, "r")
@@ -20,10 +11,8 @@ myDiction = {'a': 0}
 
 for line in lines:
     for word in line.split():
-        occurList = re.findall('\\b'+word+'\\b', textString)
+        occurList = re.findall('\\b'+word.lower()+'\\b', textString)
         occur = len(occurList)
-        if len(word) == 0:
-            occur = overlap(word, occurList)
         myDiction[word] = occur
 
 print(myDiction)
