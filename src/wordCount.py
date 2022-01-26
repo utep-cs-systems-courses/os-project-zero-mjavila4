9029f5c0d1dc136b276229899804c57d949131b7
mjavila4 <mjavila4@miners.utep.edu>
2022-01-25 23:19:22 -0700

    Working occurences

--- src/wordCount.py
+++ src/wordCount.py
@@ -1,13 +1,17 @@
 import sys
 import re
+
 fileName = sys.argv[1]
-file = open('../'+fileName, "r").read()
-myDic = {}
+file = open('../'+fileName, "r")
+fileParse = open('../'+fileName, "r")
+textString = file.read()
+lines = fileParse.readlines()
 
-x = re.findall('', file)
+myDiction = {'a':0}
 
-print(len(x))
+for line in lines:
+    for word in line.split():
+        occur = len(re.findall(word, textString))
+        myDiction[word] = occur
 
-
-
-
+print(myDiction)
